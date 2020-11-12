82c387d7398527bd6f1b8f3fe86cd947fd76fd51
LNCrafty <L.reckzulu@gmail.com>
2020-11-12 13:58:38 +0100

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -15,11 +15,11 @@
 # limitations under the License.
 
 function launchmaster() {
-  if [[ ! -e /srv/redis-master-data ]]; then
+  if [[ ! -e /redis-master-data ]]; then
     echo "Redis master data doesn't exist, data won't be persistent!"
-    mkdir /srv/redis-master-data
+    mkdir /redis-master-data
   fi
-  redis-server /srv/redis-master-op/redis.conf
+  redis-server /redis-master-op/redis.conf
 }
 
 function launchsentinel() {
@@ -39,7 +39,7 @@ function launchsentinel() {
     sleep 10
   done
 
-  sentinel_conf=/srv/redis-sentinel-op/sentinel.conf
+  sentinel_conf=/redis-sentinel-op/sentinel.conf
 
   echo "sentinel monitor mymaster ${master} 6379 2" > ${sentinel_conf}
   echo "sentinel down-after-milliseconds mymaster 60000" >> ${sentinel_conf}
@@ -66,9 +66,9 @@ function launchslave() {
     echo "Connecting to master failed.  Waiting..."
     sleep 10
   done
-  sed -i "s/%master-ip%/${master}/" /srv/redis-slave/redis.conf
-  sed -i "s/%master-port%/6379/" /srv/redis-slave/redis.conf
-  redis-server /srv/redis-slave-op/redis.conf
+  sed -i "s/%master-ip%/${master}/" /redis-slave/redis.conf
+  sed -i "s/%master-port%/6379/" /redis-slave/redis.conf
+  redis-server /redis-slave-op/redis.conf
 }
 
 if [[ "${MASTER}" == "true" ]]; then
